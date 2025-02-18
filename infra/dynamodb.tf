6e6701214afa72b54ad223a44bfc72e81b6bf954
Brad Edwards <j.bradley.edwards@gmail.com>
2025-02-18 00:59:56 -0800

    Remove manually defined state lock file

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -1,19 +1,3 @@
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-state-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = merge(local.common_tags, {
-    Name      = "terraform-state-lock"
-    Component = "state-management"
-  })
-}
-
 resource "aws_dynamodb_table" "newsletter_metadata" {
   name           = local.metadata_table_name
   billing_mode   = "PAY_PER_REQUEST"
