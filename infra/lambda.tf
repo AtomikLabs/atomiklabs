1f9127141ef1515d2ba4796cfafd0e6fc02c1f14
Brad Edwards <j.bradley.edwards@gmail.com>
2025-02-20 19:00:44 -0800

    Fix config path

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "mailer" {
 
   environment {
     variables = {
-      CONFIG_PATH = "/${var.project}/${var.environment}/arxiv"
+      CONFIG_PATH = "/${var.project}/${var.environment}"
       DEPLOY_TIMESTAMP = timestamp()
     }
   }
