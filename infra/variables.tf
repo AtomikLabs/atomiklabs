f5d19563a8df307098a677fcd0a64433f8ad0336
Brad Edwards <j.bradley.edwards@gmail.com>
2025-02-22 14:03:56 -0800

    Fix AI category not found
    Add additional extraction categories

--- infra/variables.tf
+++ infra/variables.tf
@@ -27,7 +27,7 @@ variable "arxiv_config" {
     arxiv_set  = string
   })
   default = {
-    categories = ["CL", "CV", "RO", "CR", "AI"]
+    categories = ["CL", "CV", "RO", "CR", "AI", "LG", "NE", "SY"]
     back_date  = 1
     arxiv_set  = "cs"
   }
