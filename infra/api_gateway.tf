f4ca0cf590d1ddd3163086faf8eabd8d00203cb4
Brad Edwards <j.bradley.edwards@gmail.com>
2025-03-03 21:03:22 -0800

    Add dummy endpoint for build

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -74,21 +74,71 @@ resource "aws_iam_role_policy" "api_gateway_cloudwatch" {
   })
 }
 
+resource "aws_api_gateway_resource" "dummy" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "dummy"
+}
+
+resource "aws_api_gateway_method" "dummy" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.dummy.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "dummy" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.dummy.id
+  http_method = aws_api_gateway_method.dummy.http_method
+  type        = "MOCK"
+  request_templates = {
+    "application/json" = jsonencode({
+      statusCode = 200
+    })
+  }
+}
+
+resource "aws_api_gateway_method_response" "dummy" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.dummy.id
+  http_method = aws_api_gateway_method.dummy.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "dummy" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.dummy.id
+  http_method = aws_api_gateway_method.dummy.http_method
+  status_code = aws_api_gateway_method_response.dummy.status_code
+  response_templates = {
+    "application/json" = jsonencode({
+      message = "This is a placeholder endpoint"
+    })
+  }
+}
+
 resource "aws_api_gateway_deployment" "main" {
   rest_api_id = aws_api_gateway_rest_api.main.id
 
-  # Force a new deployment when resources change
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.main.body))
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.dummy.id,
+      aws_api_gateway_method.dummy.id,
+      aws_api_gateway_integration.dummy.id
+    ]))
   }
 
   lifecycle {
     create_before_destroy = true
   }
   
-  # Ensure deployment only happens after account settings are configured
   depends_on = [
-    aws_api_gateway_account.main
+    aws_api_gateway_account.main,
+    aws_api_gateway_integration.dummy
   ]
 }
 
