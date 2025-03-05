877e6fb85b597066c4fb287c58c1d83229c8e60f
Brad Edwards <j.bradley.edwards@gmail.com>
2025-03-04 20:24:53 -0800

    API for arxiv data endpoints

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -121,6 +121,337 @@ resource "aws_api_gateway_integration_response" "dummy" {
   }
 }
 
+# API resources for the arXiv data layer
+
+# Papers resource
+resource "aws_api_gateway_resource" "papers" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "papers"
+}
+
+resource "aws_api_gateway_resource" "paper" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_resource.papers.id
+  path_part   = "{id}"
+}
+
+# Authors resource
+resource "aws_api_gateway_resource" "authors" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "authors"
+}
+
+resource "aws_api_gateway_resource" "author" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_resource.authors.id
+  path_part   = "{id}"
+}
+
+resource "aws_api_gateway_resource" "paper_authors" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_resource.paper.id
+  path_part   = "authors"
+}
+
+# Categories resource
+resource "aws_api_gateway_resource" "categories" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "categories"
+}
+
+resource "aws_api_gateway_resource" "category" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_resource.categories.id
+  path_part   = "{code}"
+}
+
+# Sets resource
+resource "aws_api_gateway_resource" "sets" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "sets"
+}
+
+resource "aws_api_gateway_resource" "set" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_resource.sets.id
+  path_part   = "{id}"
+}
+
+resource "aws_api_gateway_resource" "sets_code" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_resource.sets.id
+  path_part   = "code"
+}
+
+resource "aws_api_gateway_resource" "set_code" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_resource.sets_code.id
+  path_part   = "{code}"
+}
+
+# API methods for data layer API
+# Papers
+
+resource "aws_api_gateway_method" "get_papers" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.papers.id
+  http_method   = "GET"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+}
+
+resource "aws_api_gateway_integration" "get_papers" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.papers.id
+  http_method             = aws_api_gateway_method.get_papers.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+resource "aws_api_gateway_method" "get_paper" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.paper.id
+  http_method   = "GET"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+  request_parameters = {
+    "method.request.path.id" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "get_paper" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.paper.id
+  http_method             = aws_api_gateway_method.get_paper.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+# Authors
+resource "aws_api_gateway_method" "get_authors" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.authors.id
+  http_method   = "GET"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+}
+
+resource "aws_api_gateway_integration" "get_authors" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.authors.id
+  http_method             = aws_api_gateway_method.get_authors.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+resource "aws_api_gateway_method" "get_author" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.author.id
+  http_method   = "GET"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+  request_parameters = {
+    "method.request.path.id" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "get_author" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.author.id
+  http_method             = aws_api_gateway_method.get_author.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+resource "aws_api_gateway_method" "get_paper_authors" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.paper_authors.id
+  http_method   = "GET"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+  request_parameters = {
+    "method.request.path.paper_id" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "get_paper_authors" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.paper_authors.id
+  http_method             = aws_api_gateway_method.get_paper_authors.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+# Categories
+resource "aws_api_gateway_method" "get_categories" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.categories.id
+  http_method   = "GET"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+}
+
+resource "aws_api_gateway_integration" "get_categories" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.categories.id
+  http_method             = aws_api_gateway_method.get_categories.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+resource "aws_api_gateway_method" "get_category" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.category.id
+  http_method   = "GET"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+  request_parameters = {
+    "method.request.path.code" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "get_category" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.category.id
+  http_method             = aws_api_gateway_method.get_category.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+# Sets
+resource "aws_api_gateway_method" "get_sets" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.sets.id
+  http_method   = "GET"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+}
+
+resource "aws_api_gateway_integration" "get_sets" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.sets.id
+  http_method             = aws_api_gateway_method.get_sets.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+resource "aws_api_gateway_method" "get_set" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.set.id
+  http_method   = "GET"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+  request_parameters = {
+    "method.request.path.id" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "get_set" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.set.id
+  http_method             = aws_api_gateway_method.get_set.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+resource "aws_api_gateway_method" "get_set_by_code" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.set_code.id
+  http_method   = "GET"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+  request_parameters = {
+    "method.request.path.code" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "get_set_by_code" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.set_code.id
+  http_method             = aws_api_gateway_method.get_set_by_code.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+# Paper abstracts resource
+resource "aws_api_gateway_resource" "paper_abstract" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_resource.paper.id
+  path_part   = "abstract"
+}
+
+# Abstract API methods
+resource "aws_api_gateway_method" "get_abstract" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.paper_abstract.id
+  http_method   = "GET"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+  request_parameters = {
+    "method.request.path.id" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "get_abstract" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.paper_abstract.id
+  http_method             = aws_api_gateway_method.get_abstract.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+resource "aws_api_gateway_method" "put_abstract" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.paper_abstract.id
+  http_method   = "PUT"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+  request_parameters = {
+    "method.request.path.id" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "put_abstract" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.paper_abstract.id
+  http_method             = aws_api_gateway_method.put_abstract.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+resource "aws_api_gateway_method" "delete_abstract" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.paper_abstract.id
+  http_method   = "DELETE"
+  authorization = "NONE"  # Use IAM, Cognito, or API key in production
+  request_parameters = {
+    "method.request.path.id" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "delete_abstract" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.paper_abstract.id
+  http_method             = aws_api_gateway_method.delete_abstract.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.arxiv_api.invoke_arn
+}
+
+# Update the API Gateway deployment to include the new endpoints
+resource "aws_lambda_permission" "api_gateway_arxiv_api" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.arxiv_api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.main.execution_arn}/*/*"
+}
+
+# Update triggers for redeployment
 resource "aws_api_gateway_deployment" "main" {
   rest_api_id = aws_api_gateway_rest_api.main.id
 
@@ -128,7 +459,49 @@ resource "aws_api_gateway_deployment" "main" {
     redeployment = sha1(jsonencode([
       aws_api_gateway_resource.dummy.id,
       aws_api_gateway_method.dummy.id,
-      aws_api_gateway_integration.dummy.id
+      aws_api_gateway_integration.dummy.id,
+      # Include new resources
+      aws_api_gateway_resource.papers.id,
+      aws_api_gateway_resource.paper.id,
+      aws_api_gateway_resource.authors.id,
+      aws_api_gateway_resource.author.id,
+      aws_api_gateway_resource.paper_authors.id,
+      aws_api_gateway_resource.categories.id,
+      aws_api_gateway_resource.category.id,
+      aws_api_gateway_resource.sets.id,
+      aws_api_gateway_resource.set.id,
+      aws_api_gateway_resource.sets_code.id,
+      aws_api_gateway_resource.set_code.id,
+      # Include new methods
+      aws_api_gateway_method.get_papers.id,
+      aws_api_gateway_method.get_paper.id,
+      aws_api_gateway_method.get_authors.id,
+      aws_api_gateway_method.get_author.id,
+      aws_api_gateway_method.get_paper_authors.id,
+      aws_api_gateway_method.get_categories.id,
+      aws_api_gateway_method.get_category.id,
+      aws_api_gateway_method.get_sets.id,
+      aws_api_gateway_method.get_set.id,
+      aws_api_gateway_method.get_set_by_code.id,
+      # Include new integrations
+      aws_api_gateway_integration.get_papers.id,
+      aws_api_gateway_integration.get_paper.id,
+      aws_api_gateway_integration.get_authors.id,
+      aws_api_gateway_integration.get_author.id,
+      aws_api_gateway_integration.get_paper_authors.id,
+      aws_api_gateway_integration.get_categories.id,
+      aws_api_gateway_integration.get_category.id,
+      aws_api_gateway_integration.get_sets.id,
+      aws_api_gateway_integration.get_set.id,
+      aws_api_gateway_integration.get_set_by_code.id,
+      # Include abstract resources
+      aws_api_gateway_resource.paper_abstract.id,
+      aws_api_gateway_method.get_abstract.id,
+      aws_api_gateway_method.put_abstract.id,
+      aws_api_gateway_method.delete_abstract.id,
+      aws_api_gateway_integration.get_abstract.id,
+      aws_api_gateway_integration.put_abstract.id,
+      aws_api_gateway_integration.delete_abstract.id
     ]))
   }
 
@@ -138,7 +511,22 @@ resource "aws_api_gateway_deployment" "main" {
   
   depends_on = [
     aws_api_gateway_account.main,
-    aws_api_gateway_integration.dummy
+    aws_api_gateway_integration.dummy,
+    # Include new integrations
+    aws_api_gateway_integration.get_papers,
+    aws_api_gateway_integration.get_paper,
+    aws_api_gateway_integration.get_authors,
+    aws_api_gateway_integration.get_author,
+    aws_api_gateway_integration.get_paper_authors,
+    aws_api_gateway_integration.get_categories,
+    aws_api_gateway_integration.get_category,
+    aws_api_gateway_integration.get_sets,
+    aws_api_gateway_integration.get_set,
+    aws_api_gateway_integration.get_set_by_code,
+    # Include abstract integrations
+    aws_api_gateway_integration.get_abstract,
+    aws_api_gateway_integration.put_abstract,
+    aws_api_gateway_integration.delete_abstract
   ]
 }
 
