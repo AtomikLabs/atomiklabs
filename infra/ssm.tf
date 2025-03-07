4c42cd9881bd788a589c9f6e758929e9ffc62f54
Brad Edwards <j.bradley.edwards@gmail.com>
2025-03-06 19:44:32 -0800

    Add missing params

--- infra/ssm.tf
+++ infra/ssm.tf
@@ -1,3 +1,19 @@
+# SSM parameters for Lambda functions
+resource "aws_ssm_parameter" "s3_bucket" {
+  name  = "/${var.project}/${var.environment}/arxiv/s3_bucket"
+  type  = "String"
+  value = aws_s3_bucket.storage.id
+  tags  = local.common_tags
+}
+
+resource "aws_ssm_parameter" "arxiv_back_date" {
+  name  = "/${var.project}/${var.environment}/arxiv/back_date"
+  type  = "String"
+  value = "3"  # Default to 3 days lookback
+  tags  = local.common_tags
+}
+
+# Email configuration is still needed for the mailer function
 resource "aws_ssm_parameter" "email_recipients" {
   name  = "/${var.project}/${var.environment}/arxiv/email/recipients"
   type  = "String"
