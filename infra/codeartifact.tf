052d99f0e15c040cc4d8977f0734ae23f873e616
Brad Edwards <j.bradley.edwards@gmail.com>
2025-03-16 18:38:34 -0700

    Add code artifact for neo4j package

---
+++ infra/codeartifact.tf
@@ -0,0 +1,60 @@
+resource "aws_codeartifact_domain" "domain" {
+  domain = local.resource_prefix
+
+  tags = local.common_tags
+}
+
+resource "aws_codeartifact_repository" "neo4j_client" {
+  repository  = "neo4j-client"
+  domain      = aws_codeartifact_domain.domain.domain
+  description = "Private repository for atomiklabs Neo4j client"
+
+  tags = local.common_tags
+}
+
+resource "aws_codeartifact_repository" "python_public" {
+  repository  = "python-public"
+  domain      = aws_codeartifact_domain.domain.domain
+  description = "Public PyPI mirror"
+  
+  external_connections {
+    external_connection_name = "public:pypi"
+  }
+
+  tags = local.common_tags
+}
+
+# Use the newer resource format for upstream association
+resource "aws_codeartifact_repository_permissions_policy" "neo4j_client_policy" {
+  domain      = aws_codeartifact_domain.domain.domain
+  repository  = aws_codeartifact_repository.neo4j_client.repository
+  policy_document = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["codeartifact:ReadFromRepository"]
+        Effect   = "Allow"
+        Principal = "*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+output "codeartifact_repository" {
+  value = aws_codeartifact_repository.neo4j_client.repository
+  description = "CodeArtifact repository name"
+}
+
+output "codeartifact_domain" {
+  value = aws_codeartifact_domain.domain.domain
+  description = "CodeArtifact domain name"
+}
+
+resource "aws_ssm_parameter" "codeartifact_repo" {
+  name  = "/${var.project}/${var.environment}/codeartifact/neo4j_client_repo"
+  type  = "String"
+  value = "${aws_codeartifact_domain.domain.domain}/${aws_codeartifact_repository.neo4j_client.repository}"
+  
+  tags = local.common_tags
+}
